d03b9b91cdace74cd3e005a7cec40b000ad6e7bc
alva-prod <a00839091@tec.mx>
2025-10-11 16:39:16 -0600

    Texto con mision de Seitc

--- src/app/_components/UI/footer/Footer.tsx
+++ src/app/_components/UI/footer/Footer.tsx
@@ -13,14 +13,15 @@ const Footer = () => {
     <footer className="w-full bg-gradient-to-r from-[hsl(217,73%,16%)] via-[hsl(210,69%,27%)] to-[hsl(206,69%,44%)] text-white py-8">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {/* Company Info */}
-          <div className="space-y-4">
+          {/* SEITC Info*/}
+            <div className="space-y-4">
             <h3 className="text-xl font-bold">SEITC</h3>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Doloremque non numquam iste molestiae asperiores magnam dolor dignissimos facere quia repellat.
+            <p className="text-sm text-justify">
+              La Sociedad de Estudiantes de Ingeniería en Tecnologías Computacionales tiene como objetivo 
+              proporcionar a los estudiantes de Ingeniería Computacional la oportunidad de adquirir habilidades 
+              técnicas relevantes, conocer tecnologías emergentes y desarrollar capacidades de liderazgo y organización.
             </p>
-          </div>
+            </div>
           
           {/* Quick Links */}
           <div className="space-y-4">
